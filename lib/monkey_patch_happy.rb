8d637d83a9e086547079d211d988cab16607f57d
xdchen <xdchen@xtremeprog.com>
2016-01-12 17:58:09 +0800

    fix

--- lib/monkey_patch_happy.rb
+++ lib/monkey_patch_happy.rb
@@ -1,7 +1,7 @@
-require "monkey_patch_happy/array_patch"
-require "monkey_patch_happy/string_patch"
-require "monkey_patch_happy/hash_patch"
-require "monkey_patch_happy/nil_patch"
+# require "monkey_patch_happy/array_patch"
+# require "monkey_patch_happy/string_patch"
+# require "monkey_patch_happy/hash_patch"
+# require "monkey_patch_happy/nil_patch"
 
 #for active_record
 require "monkey_patch_happy/active_record_extras"
